9a9f9803f5fd21596187757af57533009dc9f390
M Hadi Sasmito <keselyoleren@gmail.com>
2022-07-19 21:11:01 +0700

    add tooltip in jadwal

--- app/anggota/admin.py
+++ app/anggota/admin.py
@@ -6,6 +6,14 @@ from anggota.models import Anggota
             
 class AnggotaAdmin(admin.ModelAdmin):
     list_display = [ 'avatar', 'nama', 'alamat', 'no_hp','jabatan', 'status', 'validate', 'tingkat', 'status_verify']
+    def get_queryset(self, request):
+        qs = super(AnggotaAdmin, self).get_queryset(request)
+        print(qs)        
+        return qs.filter(nama="test")
+        # if request.user.is_superuser:
+        #     return qs
+        # return qs.filter(author=request.user)
+    
     class Meta:
         model = Anggota
 
